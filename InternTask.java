f38fe25d178f152a275fa8eedd70155dd32914a7
Aleks <aleks.penelov99@gmail.com>
2024-04-25 23:20:04 +0300

    Initial Commit
    First commit containing most of Subtask 1 solution

---
+++ InternTask.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class InternTask {
+    public static int flightsMap(List<String[]> flights, String origin, String destination) {
+
+        // Create a graph representation of the flights using Hashmap
+        Map<String, List<String>> graph = new HashMap<>();
+
+        // Iterate through each flight and add it to the graph.
+        for (String[] flight : flights) {
+            String departure = flight[0];
+            String arrival = flight[1];
+            graph.putIfAbsent(departure, new ArrayList<>());
+            graph.get(departure).add(arrival);
+        }
+
+        // Perform Breadth-First Search to find the shortest travel distance
+        Queue<String[]> queue = new LinkedList<>();
+        queue.offer(new String[]{origin, "0"});
+        HashSet<String> visited = new HashSet<>();
+        while (!queue.isEmpty()) {
+            String[] current = queue.poll();
+            String currentFlight = current[0];
+            int timeSoFar = Integer.parseInt(current[1]);
+            if (currentFlight.equals(destination)) {
+                return timeSoFar;
+            }
+            if (!visited.contains(currentFlight)) {
+                visited.add(currentFlight);
+
+        // Get the other flights and add them to the queue
+                List<String> otherFlights = graph.getOrDefault(currentFlight, new ArrayList<>());
+                for (String diffFlight : otherFlights) {
+                    queue.offer(new String[]{diffFlight, String.valueOf(timeSoFar + 1)});
+                }
+            }
+        }
+
+        // If unreachable, return 0 (Subtask 1)
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        List<String[]> flights = new ArrayList<>();
+        flights.add(new String[]{"SOF", "IST"});
+        flights.add(new String[]{"IST", "CMB"});
+        flights.add(new String[]{"CMB", "MLE"});
+
+        String origin = "SOF";
+        String destination = "MLE";
+
+        System.out.println(flightsMap(flights, origin, destination));
+    }
+}
